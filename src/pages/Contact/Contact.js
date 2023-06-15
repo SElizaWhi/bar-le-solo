2f33158348196fd08ac83cd57271ddfa75cb6066
Sophie White <sophie.white@mordrid.net>
2023-06-15 15:35:46 +0200

    horaires et contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,16 +2,47 @@ import React from "react";
 import HeroImage from "../../components/HeroImage/HeroImage";
 import contact from "../../assets/contact.jpg";
 import Hours from "./Hours/Hours";
+import HeroImage2 from "../../components/HeroImage/HeroImage2";
 
 const Contact = () => {
   return (
     <div>
       <HeroImage
         menu={contact}
-        heading={["Contactez nous"]}
-        text="Contactez nous aujourd'hui pour plus de renseignements ou pour réserver"
+        heading={["Coordonnées"]}
+        text={
+          <>
+            04 30 67 09 99
+            <br />
+            2 Rue du Bon Puits, 30320 Poulx
+            <br />
+            francklenormand67@gmail.com
+            <br />
+          </>
+        }
       />
-      <Hours/>
+      <HeroImage2
+        heading={["Horaires"]}
+        text={
+          <>
+            Lundi Fermé
+            <br />
+            Mardi 7:30 - 15:00 18:00-21:30
+            <br />
+            Mercredi 7:30 - 15:00 18:00-21:30
+            <br />
+            Jeudi 7:30 - 15:00 18:00-21:30
+            <br />
+            Vendredi 7:30 - 15:00 18:00-21:30
+            <br />
+            Samedi 7:30 - 15:00 18:00-21:30
+            <br />
+            Dimanche 7:30 - 14:15
+            <br />
+            Restauration tous les midi du mardi au samedi
+          </>
+        }
+      />
     </div>
   );
 };
--- src/pages/Contact/Hours/Hours.js
+++ src/pages/Contact/Hours/Hours.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './hours.css'
+import hours from '../../../assets/hours.jpg'
 
 const Hours = () => {
   return (
-    <div>
-      
+    <div className='horaires'>
+      <img src={hours} alt="horaires" />
     </div>
-  )
+  );
 }
 
 export default Hours
